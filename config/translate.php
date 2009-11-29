efbd142d19f70a1fe5c5d2fca9483382ab8525cd
copy112 <copy112@gmail.com>
2009-11-29 12:27:38 +0100

    Added a config file.

---
+++ config/translate.php
@@ -0,0 +1,11 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+return array(
+
+	/**
+	 * Change this to your wanted password for accessing import and export functions.
+	 * Or simply modify the controller to use auth module.
+	 */
+	'password' => 'ChangeMe28',
+	
+);
