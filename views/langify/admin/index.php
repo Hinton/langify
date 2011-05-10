2a24f47ec7780138ac7517e7dbee04dc97f2aab0
Hinton <oscar@oscarhinton.com>
2011-05-10 11:12:13 +0200

    Removed the old unused views folder.

--- views/langify/admin/index.php
+++
@@ -1,1 +0,0 @@
-<p>Welcome to Langify Admin Interface, from here you will be able to modify most of your settings, and languages.</p>
--- views/langify/index.php
+++
@@ -1,7 +0,0 @@
-<p><?php echo i18n::get('langify index decscription'); ?></p>
-
-<ul class="languages">
-<?php foreach($languages as $language): ?>
-	<li><?php echo html::anchor('translate/view/'.$language->file, $language->name); ?></li>
-<?php endforeach ?>
-</ul>
